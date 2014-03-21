2edb93ae58df300bab90852c1730f4dc65c015b1
Thomas Dreibholz <dreibh@simula.no>
2014-03-22 00:24:58 +0100

    Back to workings tags.

--- lxc-tags.mk
+++ lxc-tags.mk
@@ -1,19 +1,19 @@
-lxc-userspace-GITPATH           := git://git.planet-lab.org/lxc-userspace.git@lxc-userspace-1.0-9
-transforward-GITPATH            := git://git.planet-lab.org/transforward.git@transforward-0.1-5
-procprotect-GITPATH             := git://git.planet-lab.org/procprotect.git@procprotect-0.4-3
+lxc-userspace-GITPATH           := git://git.planet-lab.org/lxc-userspace.git@master
+transforward-GITPATH            := git://git.planet-lab.org/transforward.git@transforward-0.1-4
+procprotect-GITPATH             := git://git.planet-lab.org/procprotect.git@master
 ipfw-GITPATH                    := git://git.code.sf.net/p/dummynet/code@ipfw-20130423-1
 comgt-SVNPATH			:= http://svn.onelab.eu/comgt/imports/0.3
 planetlab-umts-tools-GITPATH    := git://git.onelab.eu/planetlab-umts-tools.git@planetlab-umts-tools-0.7-1
-nodeupdate-GITPATH              := git://git.planet-lab.org/nodeupdate.git@nodeupdate-0.5-11
+nodeupdate-GITPATH              := git://git.planet-lab.org/nodeupdate.git@master
 PingOfDeath-SVNPATH		:= http://svn.planet-lab.org/svn/PingOfDeath/tags/PingOfDeath-2.2-1
 plnode-utils-GITPATH            := git://git.planet-lab.org/plnode-utils.git@plnode-utils-0.2-2
-nodemanager-GITPATH             := git://git.planet-lab.org/nodemanager.git@nodemanager-5.2-10
+nodemanager-GITPATH             := git://git.planet-lab.org/nodemanager.git@master
 #
 pl_sshd-SVNPATH			:= http://svn.planet-lab.org/svn/pl_sshd/tags/pl_sshd-1.0-11
 codemux-GITPATH			:= git://git.planet-lab.org/codemux.git@codemux-0.1-15
 fprobe-ulog-GITPATH             := git://git.planet-lab.org/fprobe-ulog.git@fprobe-ulog-1.1.4-3
-libvirt-GITPATH                 := git://git.onelab.eu/libvirt.git@libvirt-1.2.1-1
-libvirt-python-GITPATH          := git://git.onelab.eu/libvirt-python.git@libvirt-python-1.2.1-1
+libvirt-GITPATH			:= git://git.onelab.eu/libvirt.git@1.2.1
+libvirt-python-GITPATH		:= git://git.onelab.eu/libvirt-python.git@1.2.1
 pf2slice-SVNPATH		:= http://svn.planet-lab.org/svn/pf2slice/tags/pf2slice-1.0-2
 mom-GITPATH                     := git://git.planet-lab.org/mom.git@mom-2.3-5
 inotify-tools-GITPATH		:= git://git.planet-lab.org/inotify-tools.git@inotify-tools-3.13-2
@@ -21,8 +21,8 @@ openvswitch-GITPATH		:= git://git.planet-lab.org/openvswitch.git@openvswitch-1.2
 vsys-GITPATH			:= git://git.planet-lab.org/vsys.git@vsys-0.99-3
 vsys-scripts-GITPATH            := git://git.planet-lab.org/vsys-scripts.git@vsys-scripts-0.95-48
 bind_public-GITPATH             := git://git.planet-lab.org/bind_public.git@bind_public-0.1-2
-sliver-openvswitch-GITPATH      := git://git.onelab.eu/sliver-openvswitch.git@sliver-openvswitch-2.1.90-1
-plcapi-GITPATH                  := git://git.planet-lab.org/plcapi.git@plcapi-5.3-2
+sliver-openvswitch-GITPATH      := git://git.onelab.eu/sliver-openvswitch.git@sliver-openvswitch-2.0.90-1
+plcapi-GITPATH                  := git://git.planet-lab.org/plcapi.git@master
 drupal-GITPATH                  := git://git.planet-lab.org/drupal.git@drupal-4.7-15
 plewww-GITPATH                  := git://git.planet-lab.org/plewww.git@plewww-5.2-4
 www-register-wizard-SVNPATH	:= http://svn.planet-lab.org/svn/www-register-wizard/tags/www-register-wizard-4.3-5
@@ -30,28 +30,26 @@ monitor-GITPATH			:= git://git.planet-lab.org/monitor@monitor-3.1-6
 PLCRT-SVNPATH			:= http://svn.planet-lab.org/svn/PLCRT/tags/PLCRT-1.0-11
 pyopenssl-GITPATH               := git://git.planet-lab.org/pyopenssl.git@pyopenssl-0.9-2
 pyaspects-GITPATH               := git://git.planet-lab.org/pyaspects.git@pyaspects-0.4.1-3
-nodeconfig-GITPATH              := git://git.planet-lab.org/nodeconfig.git@nodeconfig-5.2-4
+nodeconfig-GITPATH              := git://git.planet-lab.org/nodeconfig.git@nodeconfig-5.2-3
 bootmanager-GITPATH             := git://git.planet-lab.org/bootmanager.git@master
 pypcilib-GITPATH                := git://git.planet-lab.org/pypcilib.git@pypcilib-0.2-11
 pyplnet-GITPATH                 := git://git.planet-lab.org/pyplnet.git@pyplnet-4.3-18
 DistributedRateLimiting-SVNPATH	:= http://svn.planet-lab.org/svn/DistributedRateLimiting/tags/DistributedRateLimiting-0.1-1
 pcucontrol-GITPATH              := git://git.planet-lab.org/pcucontrol.git@pcucontrol-1.0-13
 ###
-rvm-ruby-BRANCH			:= planetlab
 rvm-ruby-GITPATH                := git://git.onelab.eu/rvm-ruby.git@rvm-ruby-1.22.9-1
 oml-GITPATH                     := git://git.onelab.eu/oml.git@oml-2.6.1-1
 ###
-bootcd-GITPATH                  := git://git.planet-lab.org/bootcd.git@bootcd-5.2-4
+bootcd-GITPATH                  := git://git.planet-lab.org/bootcd.git@bootcd-5.2-3
 sliceimage-GITPATH              := git://git.planet-lab.org/sliceimage.git@sliceimage-5.1-6
-nodeimage-GITPATH               := git://git.planet-lab.org/nodeimage.git@nodeimage-5.2-3
-myplc-GITPATH                   := git://git.planet-lab.org/myplc.git@myplc-5.3-2
+nodeimage-GITPATH               := git://git.planet-lab.org/nodeimage.git@master
+myplc-GITPATH                   := git://git.planet-lab.org/myplc.git@master
 
 #
-sfa-BRANCH			:= geni-v3
 sfa-GITPATH                     := git://git.planet-lab.org/sfa.git@sfa-3.1-2
 #sface-GITPATH                   := git://git.planet-lab.org/sface.git@sface-0.9-9
 #
 myslice-GITPATH			:= git://git.onelab.eu/myslice.git@master
 manifold-GITPATH		:= git://git.onelab.eu/manifold.git@devel
 #
-tests-GITPATH                   := git://git.onelab.eu/tests.git@tests-5.3-2
+tests-GITPATH                   := git://git.onelab.eu/tests.git@master
