4b081f090911739f6948e18666590622d4f2b884
Thomas Dreibholz <dreibh@simula.no>
2014-07-10 11:11:05 +0200

    iptables-ipv6 is provided by iptables-services.

--- lxc.mk
+++ lxc.mk
@@ -154,7 +154,7 @@ libvirt-STOCK-DEVEL-RPMS += ceph-devel numactl-devel libcap-ng-devel scrub
 libvirt-STOCK-DEVEL-RPMS += libblkid-devel glusterfs-api-devel glusterfs-devel
 # strictly speaking fuse-devel is not required anymore but we might wish to turn fuse back on again in the future
 libvirt-STOCK-DEVEL-RPMS += fuse-devel libssh2-devel dbus-devel numad 
-libvirt-STOCK-DEVEL-RPMS += systemd-devel libnl3-devel iptables-ipv6 libgcrypt-devel netcf-devel
+libvirt-STOCK-DEVEL-RPMS += systemd-devel libnl3-devel iptables-services libgcrypt-devel netcf-devel
 ALL += libvirt
 IN_NODEREPO += libvirt
 IN_NODEIMAGE += libvirt
