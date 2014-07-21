de62841ab7dddd0cbe4b24527176a65aa7945d41
Thomas Dreibholz <dreibh@simula.no>
2014-07-21 13:20:11 +0200

    Trying fixed sliceimage.

--- nornet-tags.mk
+++ nornet-tags.mk
@@ -79,7 +79,7 @@ pcucontrol-GITPATH              := git://git.onelab.eu/pcucontrol.git@pcucontrol
 oml-GITPATH                     := git://git.onelab.eu/oml.git@oml-2.6.1-1
 ###
 # !!! Using NorNet customisation! !!! bootcd-GITPATH                  := git://git.planet-lab.org/bootcd.git@bootcd-5.2-4
-sliceimage-GITPATH              := git://git.onelab.eu/sliceimage.git@sliceimage-5.1-8
+sliceimage-GITPATH              := git://git.onelab.eu/sliceimage.git@master
 nodeimage-GITPATH               := git://git.onelab.eu/nodeimage.git@nodeimage-5.2-3
 myplc-GITPATH                   := git://git.onelab.eu/myplc.git@myplc-5.3-2
 
