4ce764e2a7f998e223d00c2c26dbc7dc28782c71
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-06-27 14:35:28 +0200

    f24

--- pkgs.py
+++ pkgs.py
@@ -35,13 +35,13 @@ import re
 
 default_arch='x86_64'
 known_arch = ['i386', 'i686', 'x86_64']
-default_fcdistro = 'f23'
+default_fcdistro = 'f24'
 known_fcdistros = [
     'centos5', 'centos6',
     # oldies but we have references to that in the pkgs files
     'f8', 'f10', 'f12', 'f16',
     # these ones are still relevant
-    'f14', 'f18', 'f20', 'f21', 'f22', 'f23',
+    'f14', 'f18', 'f20', 'f21', 'f22', 'f23', 'f24',
     'sl6', 
     # debians
     'wheezy','jessie',
