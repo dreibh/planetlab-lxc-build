6202299ceb5226a3b1809d343a156a82c7fe779d
Thomas Dreibholz <dreibh@simula.no>
2016-11-04 12:37:35 +0100

    Updated tags.

--- nornet-tags.mk
+++ nornet-tags.mk
@@ -18,10 +18,10 @@
 nornet-ca-GITPATH               := git://github.com/dreibh/nornet-ca.git@nornet-rel1.0.0
 nornet-nn-GITPATH               := git://github.com/dreibh/nornet-nn.git@nornet-rel1.0.0
 
-netperfmeter-GITPATH            := git://github.com/dreibh/netperfmeter.git@netperfmeter-1.5.6
+netperfmeter-GITPATH            := git://github.com/dreibh/netperfmeter.git@netperfmeter-1.6.1
 rsplib-GITPATH                  := git://github.com/dreibh/rsplib.git@rsplib-3.1.1
-subnetcalc-GITPATH              := git://github.com/dreibh/subnetcalc.git@subnetcalc-2.4.4
-tsctp-GITPATH                   := git://github.com/dreibh/tsctp.git@tsctp-0.6.2
+subnetcalc-GITPATH              := git://github.com/dreibh/subnetcalc.git@subnetcalc-2.4.5
+tsctp-GITPATH                   := git://github.com/dreibh/tsctp.git@tsctp-0.6.3
 # ###########################################################################
 
 # ##### NorNet ########################
