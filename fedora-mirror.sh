d848dcd0bb914e82e8ddd76e4bfb737136de3c3f
parmentelat <thierry.parmentelat@inria.fr>
2018-11-26 14:05:40 +0100

    welcome f29, by f25

--- fedora-mirror.sh
+++ fedora-mirror.sh
@@ -16,8 +16,8 @@ root=/mirror/
 us_fedora_url=rsync://mirrors.kernel.org/fedora
 eu_fedora_url=rsync://mirror1.hs-esslingen.de/fedora/linux
 
-default_distroname="f27"
-all_distronames="f25 f27"
+default_distroname="f29"
+all_distronames="f27 f29"
 
 global_arch="x86_64"
 
