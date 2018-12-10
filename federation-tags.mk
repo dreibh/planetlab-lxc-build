9e51b9fef759866dc344a96bbbb4bbed845e7bbf
parmentelat <thierry.parmentelat@inria.fr>
2018-12-10 17:37:14 +0100

    branch geni-v3 in sfa has gone, just use plain master

--- federation-tags.mk
+++ federation-tags.mk
@@ -1,4 +1,3 @@
-sfa-BRANCH			:= geni-v3
-sfa-GITPATH                     := git://git.planet-lab.org/sfa.git@geni-v3
+sfa-GITPATH                     := git://git.planet-lab.org/sfa.git@master
 myslice-GITPATH			:= git://git.onelab.eu/myslice.git@onelab
 manifold-GITPATH		:= git://git.onelab.eu/manifold.git@devel
--- nornet-tags.mk
+++ nornet-tags.mk
@@ -84,7 +84,6 @@ nodeimage-GITPATH               := git://git.onelab.eu/nodeimage.git@nodeimage-5
 myplc-GITPATH                   := git://git.onelab.eu/myplc.git@myplc-5.3-3
 
 #
-sfa-BRANCH			:= geni-v3
 sfa-GITPATH                     := git://git.onelab.eu/sfa.git@sfa-3.1-13
 #
 tests-GITPATH                   := git://git.onelab.eu/tests.git@tests-6.0-4
