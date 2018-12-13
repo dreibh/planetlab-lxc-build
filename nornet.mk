9d89b0807fc09ed23098f3694d45f469d9e4c3ee
Thomas Dreibholz <dreibh@simula.no>
2018-12-13 13:00:55 +0100

    Merged upstream changes.

--- nornet.mk
+++ nornet.mk
@@ -147,7 +147,7 @@ IN_NODEIMAGE += transforward
 # procprotect: root context module for protecting against weaknesses in /proc
 #
 ### remove procprotect from the nodes on f20 and above, needs more work starting with 3.19
-ifneq "$(DISTRONAME)" "$(filter $(DISTRONAME),f20 f21 f22 f23 f24 f25 f27)"
+ifneq "$(DISTRONAME)" "$(filter $(DISTRONAME),f20 f21 f22 f23 f24 f25 f27 f29)"
 procprotect-MODULES := procprotect
 procprotect-SPEC := procprotect.spec
 # ##### NorNet ########################
@@ -165,7 +165,7 @@ endif
 #
 ### starting August 2015, ipfw module won't build against fedora22
 # that comes with kernel 4.1.4
-ifneq "$(DISTRONAME)" "$(filter $(DISTRONAME),f21 f22 f23 f24 f25 f27)"
+ifneq "$(DISTRONAME)" "$(filter $(DISTRONAME),f21 f22 f23 f24 f25 f27 f29)"
 ipfwroot-MODULES := ipfw
 ipfwroot-SPEC := planetlab/ipfwroot.spec
 # ##### NorNet ########################
@@ -264,7 +264,7 @@ IN_NODEIMAGE += codemux
 # fprobe-ulog
 #
 # xxx temporarily turning this off on f20 and above
-ifneq "$(DISTRONAME)" "$(filter $(DISTRONAME),f20 f21 f22 f23 f24 f25 f27)"
+ifneq "$(DISTRONAME)" "$(filter $(DISTRONAME),f20 f21 f22 f23 f24 f25 f27 f29)"
 fprobe-ulog-MODULES := fprobe-ulog
 fprobe-ulog-SPEC := fprobe-ulog.spec
 ALL += fprobe-ulog
@@ -404,6 +404,9 @@ ALL += vsys-wrapper
 # ALL += bind_public
 # ##### NorNet ########################
 
+# in fedora 29, this triggers nasty-looking compile messages
+# not trying too hard, we're mostly after the server-side of f29
+ifneq "$(DISTRONAME)" "$(filter $(DISTRONAME), f29)"
 #
 # sliver-openvswitch
 #
@@ -411,6 +414,7 @@ sliver-openvswitch-MODULES := sliver-openvswitch
 sliver-openvswitch-SPEC := sliver-openvswitch.spec
 IN_SLICEIMAGE += sliver-openvswitch
 ALL += sliver-openvswitch
+endif
 
 #
 # plcapi
@@ -523,7 +527,7 @@ IN_NODEIMAGE += pyplnet
 IN_MYPLC += pyplnet
 IN_BOOTCD += pyplnet
 
-ifneq "$(DISTRONAME)" "$(filter $(DISTRONAME),f23 f24 f25 f27)"
+ifneq "$(DISTRONAME)" "$(filter $(DISTRONAME),f23 f24 f25 f27 f29)"
 #
 # OML measurement library
 #
