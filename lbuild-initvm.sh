ccdbfde0e9cda749d790f901247f1c524177c951
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-11-27 09:26:09 +0100

    attempt to start the network in the f31 vms

--- lbuild-initvm.sh
+++ lbuild-initvm.sh
@@ -312,7 +312,8 @@ function fedora_configure_systemd() {
     ln -sf /dev/null ${lxc_root}/etc/systemd/system/"getty@.service"
     rm -f ${lxc_root}/etc/systemd/system/getty.target.wants/*service || :
 # can't seem to handle this one with systemctl
-    chroot ${lxc_root} $personality chkconfig network on
+# second part should trigger starting with fedora31, where the network target is not manually manageable
+    chroot ${lxc_root} $personality chkconfig network on ||     chroot ${lxc_root} $personality systemctl enable NetworkManager
 }
 
 # overwrite container yum config
