ca459c433168a64ace75c135c537d8fdadd40a98
Thomas Dreibholz <dreibh@simula.no>
2022-05-03 17:01:42 +0200

    Merged upstream changes.

--- nornet-tags.mk
+++ nornet-tags.mk
@@ -26,12 +26,12 @@ tsctp-GITPATH                   := git://github.com/dreibh/tsctp.git@master
 
 # ##### NorNet ########################
 bootmanager-GITPATH             := git://github.com/dreibh/planetlab-bootmanager.git@master
-linux-GITPATH                   := git://github.com/dreibh/planetlab-kernel.git@kernel-v4.19
 plcapi-GITPATH                  := git://github.com/dreibh/planetlab-lxc-plcapi.git@master
 plewww-GITPATH                  := git://github.com/dreibh/planetlab-lxc-plewww@master
 bootcd-GITPATH                  := git://github.com/dreibh/planetlab-lxc-bootcd.git@master
 nodemanager-GITPATH             := git://github.com/dreibh/planetlab-lxc-nodemanager.git@master
 tests-GITPATH                   := git://github.com/dreibh/planetlab-lxc-tests.git@master
+linux-GITPATH                   := git://github.com/dreibh/planetlab-kernel.git@kernel-v4.19
 # #####################################
 
 lxc-userspace-GITPATH       := git://git.onelab.eu/lxc-userspace.git@lxc-userspace-2.0-0
@@ -41,8 +41,8 @@ planetlab-umts-tools-GITPATH:= git://git.onelab.eu/planetlab-umts-tools.git@plan
 nodeupdate-GITPATH          := git://git.onelab.eu/nodeupdate.git@nodeupdate-1.0-0
 PingOfDeath-GITPATH         := git://git.onelab.eu/pingofdeath.git@master
 plnode-utils-GITPATH        := git://git.onelab.eu/plnode-utils.git@plnode-utils-1.0-0
-# !!! Using NorNet customisation! !!! nodemanager-GITPATH             := git://git.onelab.eu/nodemanager.git@nodemanager-master
-#
+# !!! Using NorNet customisation! !!! nodemanager-GITPATH             := git://git.onelab.eu/nodemanager.git@master
+
 pl_sshd-GITPATH             := git://git.onelab.eu/pl_sshd.git@pl_sshd-1.0-11
 codemux-GITPATH             := git://git.onelab.eu/codemux.git@codemux-0.1-15
 libvirt-GITPATH             := git://git.onelab.eu/libvirt.git@libvirt-1.2.11-2
@@ -53,22 +53,22 @@ vsys-GITPATH                    := git://git.onelab.eu/vsys.git@master
 vsys-scripts-GITPATH            := git://git.onelab.eu/vsys-scripts.git@master
 bind_public-GITPATH         := git://git.onelab.eu/bind_public.git@bind_public-0.1-3
 sliver-openvswitch-GITPATH  := git://git.onelab.eu/sliver-openvswitch.git@sliver-openvswitch-2.2.90-1
-# !!! Using NorNet customisation! !!! plcapi-GITPATH                  := git://git.onelab.eu/plcapi.git@plcapi-master
+# !!! Using NorNet customisation! !!! plcapi-GITPATH                  := git://git.onelab.eu/plcapi.git@master
 drupal-GITPATH              := git://git.onelab.eu/drupal.git@drupal-4.7-17
-# !!! Using NorNet customisation! !!! plewww-GITPATH                  := git://git.onelab.eu/plewww.git@plewww-master
+plewww-GITPATH                  := git://git.onelab.eu/plewww.git@master
 www-register-wizard-GITPATH := git://git.onelab.eu/www-register-wizard.git@www-register-wizard-4.3-5
 monitor-GITPATH             := git://git.onelab.eu/monitor@monitor-3.1-6
 PLCRT-GITPATH               := git://git.onelab.eu/plcrt.git@master
 nodeconfig-GITPATH          := git://git.onelab.eu/nodeconfig.git@nodeconfig-5.2-5
-# !!! Using NorNet customisation! !!! bootmanager-GITPATH             := git://git.onelab.eu/bootmanager.git@bootmanager-master
+# !!! Using NorNet customisation! !!! bootmanager-GITPATH             := git://git.onelab.eu/bootmanager.git@master
 pypcilib-GITPATH                := git://git.onelab.eu/pypcilib.git@master
 pyplnet-GITPATH             := git://git.onelab.eu/pyplnet.git@pyplnet-7.0-0
 pcucontrol-GITPATH              := git://git.onelab.eu/pcucontrol.git@master
-# !!! Using NorNet customisation! !!! bootcd-GITPATH                  := git://git.onelab.eu/bootcd.git@bootcd-master
-sliceimage-GITPATH          := git://git.onelab.eu/sliceimage.git@sliceimage-5.1-12
+# !!! Using NorNet customisation! !!! bootcd-GITPATH                  := git://git.onelab.eu/bootcd.git@master
+sliceimage-GITPATH              := git://git.onelab.eu/sliceimage.git@master
 nodeimage-GITPATH               := git://git.onelab.eu/nodeimage.git@master
 myplc-GITPATH                   := git://git.onelab.eu/myplc.git@master
 #
 sfa-GITPATH                     := git://git.onelab.eu/sfa.git@python2
-
-# !!! Using NorNet customisation! !!! tests-GITPATH                   := git://git.onelab.eu/tests.git@tests-master
+#
+# !!! Using NorNet customisation! !!! tests-GITPATH               := git://git.onelab.eu/tests.git@master
